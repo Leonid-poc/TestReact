ea57cd63f2ca4a9ccc9272c2080a41ba66d210e4
Leonid-poc <legeorg2004@gmail.com>
2023-10-21 02:03:20 +0500

    Добавление конструктора в класс, а также готовая форма добавления пользователя

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,15 +1,32 @@
 import React from "react";
 
 class AddUser extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstName: '',
+            lastName: '',
+            age: '',
+            bio: '',
+            price: '',
+        }
+    }
+
     render() {
         return (
             <form>
-                <input type="text" placeholder="Имя"/>
-                <input type="text" placeholder="Фамилия"/>
-                <input type="text" placeholder="О Себе"/>
-                <input type="text" placeholder="Возраст"/>
-                <label htmlFor="Happy">Бесценный?  <input type="checkbox" id="Happy"/></label>
-                <button type="button">Добавить</button>
+                <input type="text" onChange={(e) => {this.setState({firstName: e.target.value})}} placeholder="Имя"/>
+                <input type="text" onChange={(e) => {this.setState({lastName: e.target.value})}} placeholder="Фамилия"/>
+                <input type="text" onChange={(e) => {this.setState({bio: e.target.value})}} placeholder="О Себе"/>
+                <input type="text" onChange={(e) => {this.setState({age: e.target.value})}} placeholder="Возраст"/>
+                <label htmlFor="Price">Бесценный?  <input type="checkbox" id="Price"onChange={(e) => {this.setState({price: e.target.checked ? "Infinity" : "0$"})}}/></label>
+                <button type="button" onClick={() => {this.props.add({
+                    firstName: this.state.firstName,
+                    lastName: this.state.lastName,
+                    price: this.state.price,
+                    bio: this.state.bio,
+                    age: this.state.age
+                })}}>Добавить</button>
             </form>
         );
     }
