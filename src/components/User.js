bd1c2140a899c1c897a9bf1b0ea0bf0bb866401e
Leonid-poc <legeorg2004@gmail.com>
2023-10-21 17:08:02 +0500

    add new icons for delete and edit user

--- src/components/User.js
+++ src/components/User.js
@@ -1,18 +1,26 @@
 import React from "react";
+import AddUser from "./AddUser";
+import { AiFillCloseCircle, AiFillEdit } from "react-icons/ai";
 
 class User extends React.Component {
     constructor(props) {
         super(props);
         this.userinfo = props.user;
+        this.state = {
+            editing: false,
+        }
     }
 
     render() {
         return (
             <div className="userInfo" key={this.userinfo.id}>
+                <AiFillCloseCircle className="EventBut"onClick={() => {this.props.onDelete(this.userinfo.id) }}/>
+                <AiFillEdit className="EventBut" onClick={() => {this.setState({editing: !this.state.editing})}}/>
                 <h3>{this.userinfo.lastName} {this.userinfo.firstName}</h3>
                 <p>{this.userinfo.bio.length > 100 ? this.userinfo.bio.substring(0, 100) + '...' : this.userinfo.bio}</p>
                 <hr></hr>
                 <div className="userPrice">Price: {this.userinfo.price}</div>
+                {this.state.editing ? <AddUser/> : ""}
             </div>
         )
     }
