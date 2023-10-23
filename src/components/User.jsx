e2b7522894cb808dcd82fb9a8e1afdf81b94b042
Leonid-poc <legeorg2004@gmail.com>
2023-10-23 21:26:07 +0500

    refactor for state

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,23 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AddUser from "./AddUser";
 import { AiFillCloseCircle, AiFillEdit } from "react-icons/ai";
 
 const User = (props) => {
-    const [state, setState] = useState(0);
+    const [edit, setEdit] = useState(false);
     const userinfo = props.user;
-    setState({
-        editing: false,
-    });
+
 
     return (
         <div className="userInfo" key={userinfo.id}>
             <AiFillCloseCircle className="EventBut"onClick={() => {props.onDelete(userinfo.id) }}/>
-            <AiFillEdit className="EventBut" onClick={() => {setState({editing: !state.editing})}}/>
+            <AiFillEdit className="EventBut" onClick={setEdit(!edit)}/>
             <h3>{userinfo.lastName} {userinfo.firstName}</h3>
             <p>{userinfo.bio.length > 100 ? userinfo.bio.substring(0, 100) + '...' : userinfo.bio}</p>
             <hr></hr>
             <div className="userPrice">Price: {userinfo.price ? "Infinity" : "0$"}</div>
-            {state.editing && <AddUser editing={true} edit={props.onEdit} user_info={userinfo}/>}
+            {edit && <AddUser editing={true} edit={props.onEdit} user_info={userinfo}/>}
         </div>
     )
 }
