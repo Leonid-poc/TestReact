8e7d925cb075696d3c93a16b5493cf3fc580d00f
Leonid-poc <legeorg2004@gmail.com>
2023-10-26 13:31:56 +0500

    оптимизация сокращения биографии

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,18 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import AddUser from "./AddUser";
 import { AiFillCloseCircle, AiFillEdit } from "react-icons/ai";
 
 const User = (props) => {
     const [edit, setEdit] = useState(false);
-    const userinfo = props.user;
-
+    const userinfo = props.user
+    useEffect(() => {
+        setEdit(false);
+    });
 
     return (
         <div className="userInfo" key={userinfo.id}>
             <AiFillCloseCircle className="EventBut"onClick={() => {props.onDelete(userinfo.id) }}/>
-            <AiFillEdit className="EventBut" onClick={setEdit(!edit)}/>
+            <AiFillEdit className="EventBut" onClick={() => setEdit(!edit)}/>
             <h3>{userinfo.lastName} {userinfo.firstName}</h3>
-            <p>{userinfo.bio.length > 100 ? userinfo.bio.substring(0, 100) + '...' : userinfo.bio}</p>
+            <p>{(userinfo.bio && userinfo.bio.length > 100) ? userinfo.bio.substring(0, 100) + '...' : userinfo.bio}</p>
             <hr></hr>
             <div className="userPrice">Price: {userinfo.price ? "Infinity" : "0$"}</div>
             {edit && <AddUser editing={true} edit={props.onEdit} user_info={userinfo}/>}
